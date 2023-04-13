dc2ca4494e33490baf357e23967272647240f01b
Luan Victor <luanvictor2108@gmail.com>
2023-04-12 22:54:24 -0300

    Feat: Finalizando build 1 do projeto e pondo no ar

--- luan-dev-portifolio/src/Components/Home.projects.js
+++ luan-dev-portifolio/src/Components/Home.projects.js
@@ -1,4 +1,8 @@
 import React from "react";
+import dyroImage from '../Images/projetos/dyro/dyro-tela-pesquisa.png'
+import tfcImage from '../Images/projetos/tfc/tfc-tabela-visitantes.png'
+import tryfoodImage from '../Images/projetos/tryfood/tryfood-filter-page.png'
+import '../css/home-project.css'
 
 function HomeProjects () {
   return (
@@ -6,18 +10,18 @@ function HomeProjects () {
       <div className="projects">
         <h2>Aqui estão alguns dos meus projetos</h2>
         <div className="projects-carts">
-        <div>
-          <img className="image-project" src="" alt="projeto 1" />
-          <h4>Recipes App</h4>
-        </div>
-        <div>
-          <img className="image-project" src="" alt="projeto 2"/>
-          <h4>Trybe Tunes</h4>
-        </div>
-        <div>
-          <img className="image-project" src="" alt="projeto 3" />
-          <h4>FrontEnd Store</h4>
-        </div>
+        <a href="https://github.com/LuanVictr/Trybe_Recipes_App"><div className="project-container">
+          <img className="image-project-mobile" src={tryfoodImage} alt="tryfood images" />
+          <h4>Tryfood</h4>
+        </div></a>
+        <a href="https://github.com/LuanVictr/Projeto_Trybetunes"><div className="project-container">
+          <img className="image-project" src={dyroImage} alt="projeto 2"/>
+          <h4>Dyro</h4>
+        </div></a>
+        <a href=""><div className="project-container">
+          <img className="image-project" src={tfcImage} alt="projeto 3" />
+          <h4>Trybe Futebol Clube</h4>
+        </div></a>
         </div>
       </div>
     </div>
